5964dacfb981e54f51934c95545d65770faaa68e
Maazzii <tlwlro@outlook.com>
2022-06-09 17:42:58 +0900

    4장까지 완료

--- src/main/java/com/example/project/springboot/service/posts/PostsService.java
+++ src/main/java/com/example/project/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.example.project.springboot.service.posts;
 
 import com.example.project.springboot.domain.posts.Posts;
 import com.example.project.springboot.domain.posts.PostsRepository;
+import com.example.project.springboot.web.dto.PostsListResponseDto;
 import com.example.project.springboot.web.dto.PostsResponseDto;
 import com.example.project.springboot.web.dto.PostsSaveRequestDto;
 import com.example.project.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);  // JpaRepository에서 지원하는 delete 메소드 활용 / deleteById 메소드를 이용해 id로 삭제할 수도 있음
     }
 }
