b2c600ca6db6597256f0da995ab66b999c1192c8
Maazzii <tlwlro@outlook.com>
2022-06-13 17:20:00 +0900

    카카오 로그인 구현

--- src/main/java/com/example/project/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/example/project/springboot/config/auth/dto/OAuthAttributes.java
@@ -30,6 +30,8 @@ public class OAuthAttributes {
 
         if ("naver".equals(registrationId)) {
             return ofNaver("id", attributes);
+        } else if ("kakao".equals(registrationId)) {
+            return ofKakao("id", attributes);
         }
 
         return ofGoogle(userNameAttributeName, attributes);
@@ -48,11 +50,26 @@ public class OAuthAttributes {
     public static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
         Map <String, Object> response = (Map<String, Object>) attributes.get("response");
 
+        System.out.println(response.toString());
+
         return OAuthAttributes.builder()
                 .name((String) response.get("name"))
                 .email((String) response.get("email"))
                 .picture((String) response.get("profile_image"))
                 .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        Map <String, Object> response = (Map<String, Object>) attributes.get("kakao_account");
+        Map <String, Object> profile = (Map<String, Object>) response.get("profile");
+
+        return OAuthAttributes.builder()
+                .name((String) profile.get("nickname"))
+                .email((String) response.get("email"))
+                .picture((String) profile.get("profile_image_url"))
+                .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
