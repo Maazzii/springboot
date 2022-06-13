af64c82acd6b6765c3dddf2c53d5b486efc8319b
Maazzii <tlwlro@outlook.com>
2022-06-13 15:58:38 +0900

    update switch.sh

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -11,5 +11,5 @@ function switch_proxy() {
   echo "> Port 전환"
   echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
   echo "> 엔진엑스 Reload"
-  sudo seervice nginx reload
+  sudo systemctl reload nginx
 }
